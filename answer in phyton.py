6675aa47c547e707d54ea82853998719caf43113
BARAK ZUARETS <84792822+barakZuarets@users.noreply.github.com>
2023-01-22 14:44:53 +0200

    Update answer in phyton.py

--- answer in phyton.py
+++ answer in phyton.py
@@ -1,4 +1,4 @@
-1)
+# 1)
 
 def priceCheck( products, productPrices, productSold, soldPrice):
     counter=0
@@ -22,43 +22,53 @@ productSold=['rice', 'cheese']
 soldPrice=[18.99, 400.89]
 
 print(priceCheck( products, productPrices, productSold, soldPrice) )
-2)
-CREATE TABLE EMPLOYEE (
-  ID INTEGER PRIMARY KEY,
-  NAME TEXT NOT NULL,
-  DEPT_ID INTEGER NOT NULL
-);
--- insert some values
-INSERT INTO EMPLOYEE VALUES (1, 'Candice', 1);
-INSERT INTO EMPLOYEE VALUES (2, 'Julia', 2);
-INSERT INTO EMPLOYEE VALUES (3, 'Bob', 4);
-INSERT INTO EMPLOYEE VALUES (4, 'Scarlet', 1);
-INSERT INTO EMPLOYEE VALUES (5, 'Ileana', 4);
+# 2)
+# CREATE TABLE EMPLOYEE (
+#   ID INTEGER PRIMARY KEY,
+#   NAME TEXT NOT NULL,
+#   DEPT_ID INTEGER NOT NULL
+# );
+# -- insert some values
+# INSERT INTO EMPLOYEE VALUES (1, 'Candice', 1);
+# INSERT INTO EMPLOYEE VALUES (2, 'Julia', 2);
+# INSERT INTO EMPLOYEE VALUES (3, 'Bob', 4);
+# INSERT INTO EMPLOYEE VALUES (4, 'Scarlet', 1);
+# INSERT INTO EMPLOYEE VALUES (5, 'Ileana', 4);
+# 
+# CREATE TABLE DEPARTMENT (
+#   ID INTEGER PRIMARY KEY,
+#   NAME TEXT NOT NULL
+# );
+# -- insert some values
+# INSERT INTO DEPARTMENT VALUES (1, 'Executive');
+# INSERT INTO DEPARTMENT VALUES (2, 'Production');
+# INSERT INTO DEPARTMENT VALUES (3, 'Resources');
+# INSERT INTO DEPARTMENT VALUES (4, 'Technical');
+# INSERT INTO DEPARTMENT VALUES (5, 'Management');
+# -- fetch some values
+# SELECT DEPARTMENT.NAME ,COUNT(EMPLOYEE.ID)  FROM DEPARTMENT 
+# LEFT JOIN EMPLOYEE  ON DEPARTMENT.ID = EMPLOYEE.DEPT_ID
+# GROUP BY DEPARTMENT.NAME
+# ORDER BY COUNT(EMPLOYEE.ID) DESC,  DEPARTMENT.NAME ASC;
+# 
+# 
 
-CREATE TABLE DEPARTMENT (
-  ID INTEGER PRIMARY KEY,
-  NAME TEXT NOT NULL
-);
--- insert some values
-INSERT INTO DEPARTMENT VALUES (1, 'Executive');
-INSERT INTO DEPARTMENT VALUES (2, 'Production');
-INSERT INTO DEPARTMENT VALUES (3, 'Resources');
-INSERT INTO DEPARTMENT VALUES (4, 'Technical');
-INSERT INTO DEPARTMENT VALUES (5, 'Management');
--- fetch some values
-SELECT DEPARTMENT.NAME ,COUNT(EMPLOYEE.ID)  FROM DEPARTMENT 
-LEFT JOIN EMPLOYEE  ON DEPARTMENT.ID = EMPLOYEE.DEPT_ID
-GROUP BY DEPARTMENT.NAME
-ORDER BY COUNT(EMPLOYEE.ID) DESC,  DEPARTMENT.NAME ASC;
-
-
-
-3)
+# 3)
 def summer(num):
     if (num<10):
         return num
     return (num %10 + summer(int(num / 10)))
 
 print(summer(2347623))
-4)
-int(input("enter a number: "))
+# 4)
+temp = int(input("enter a number: "))
+max=0
+count=0
+while (temp != 0):
+    if (temp > max):
+        max=temp
+        count = int(1)
+    elif (temp == max):
+        count = count + 1
+    temp = int(input("enter a number: "))
+print(f'sequence maximum is: {max} and we have {count} {max}s in the sequence.')
